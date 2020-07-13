ada1d14106a5374144bf0926f2ebac80fea6e20d
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-07-13 04:42:17 +0000

    initial commit

---
+++ Web Quiz Engine/task/src/engine/Quiz.java
@@ -0,0 +1,41 @@
+/*
+package engine;
+
+public class Quiz {
+    private String title;
+    private String text;
+    private String[] options;
+
+
+    public Quiz() {
+        title = "The Java Logo";
+        text = "What is depicted on the Java Logo?";
+        options = new String[]{"Robot", "The Tea leaf", "Cup of coffee", "Bug"};
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String[] getOptions() {
+        return options;
+    }
+
+    public void setOptions(String[] options) {
+        this.options = options;
+    }
+}
+*/
---
+++ Web Quiz Engine/task/src/engine/QuizController.java
@@ -0,0 +1,21 @@
+/*
+package engine;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class QuizController {
+
+
+    @GetMapping(path = "api/quiz")
+    private Quiz returnJsonQuiz() {
+        return new Quiz();
+    }
+
+    */
+/*@PostMapping(path = "api/quiz/{answer}")
+    private String*//*
+
+
+}
+*/
