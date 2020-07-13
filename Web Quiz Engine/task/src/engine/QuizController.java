b8535b0bd59c85a20e219edabf3d4d1acb203c04
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-07-13 13:46:40 +0000

    create quizResult object for returning quiz results

--- Web Quiz Engine/task/src/engine/QuizController.java
+++ Web Quiz Engine/task/src/engine/QuizController.java
@@ -4,16 +4,24 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class QuizController {
+    QuizResults quizResults = new QuizResults();
 
     @GetMapping(path = "api/quiz")
     private Quiz returnJsonQuiz() {
         return new Quiz();
     }
 
-
-/*@PostMapping(path = "api/quiz/{answer}")
-    private String*/
-
+    @PostMapping(path = "api/quiz")
+    private QuizResults postQuizAnswer(@RequestParam(value = "answer") int answer){
+        if (answer != 2) {
+            quizResults.setFeedback("Wrong answer! Please try again.");
+            quizResults.setSuccess(false);
+        } else {
+            quizResults.setFeedback("Congratulations, you're right!");
+            quizResults.setSuccess(true);
+        }
+        return quizResults ;
+    }
 
 }
 
