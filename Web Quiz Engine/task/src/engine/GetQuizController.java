231a41ec32f4cb48bc7096d64f329445b4c46d58
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-07-31 11:54:55 +0000

    remove createNewQuestion and get quiz from QuizDB

--- Web Quiz Engine/task/src/engine/GetQuizController.java
+++ Web Quiz Engine/task/src/engine/GetQuizController.java
@@ -5,22 +5,16 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static engine.QuizDB.*;
+
 @RestController
 public class GetQuizController {
-    private List<Quiz> mQuiz = new ArrayList();
 
     public GetQuizController() {
-
     }
 
     @GetMapping(path = "api/quizzes/{id}")
     public Quiz getQuiz(@PathVariable int id) {
-        return mQuiz.get(id -1);
-    }
-
-    @PostMapping(path = "api/quizzes", consumes = "application/json")
-    private String createNewQuestion(@RequestBody Quiz quiz){
-        mQuiz.add(quiz);
-        return "posted" + mQuiz.size();
+        return getAQuiz(id);
     }
 }
