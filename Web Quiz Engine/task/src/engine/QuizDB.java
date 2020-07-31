ab9176fb988cebba431b7548833739cf3bab940f
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-07-31 11:52:20 +0000

    create quizDB for storing all quizzes

---
+++ Web Quiz Engine/task/src/engine/QuizDB.java
@@ -0,0 +1,28 @@
+package engine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuizDB {
+    private static List<Quiz> quizDB;
+
+    public static void  createQuizDB() {
+        quizDB = new ArrayList<>();
+    }
+
+    public static List<Quiz> getQuizDB() {
+        return quizDB;
+    }
+
+    public static Quiz getAQuiz(int index) {
+        return quizDB.get(index - 1);
+    }
+
+    public static void addQuiz(Quiz quiz) {
+        quizDB.add(quiz);
+    }
+
+    public static int sizeOfQuizDB() {
+        return quizDB.size();
+    }
+}
