bdcb0081e6d4c29a5bd4bf86dab328e28b5137c0
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-08-04 09:57:56 +0000

    throw exception if quiz is not available

--- Web Quiz Engine/task/src/engine/GetQuizController.java
+++ Web Quiz Engine/task/src/engine/GetQuizController.java
@@ -1,19 +1,23 @@
 package engine;
 
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 import static engine.QuizDB.*;
 
 @RestController
 public class GetQuizController {
-
     public GetQuizController() {
     }
 
     @GetMapping(path = "api/quizzes/{id}")
-    public Quiz getQuiz(@PathVariable int id) {
-        return getAQuiz(id);
+    public Quiz getQuiz(@PathVariable int id) throws QuizNotFoundException {
+        if (id < sizeOfQuizDB() || id > sizeOfQuizDB()) {
+            throw new QuizNotFoundException();
+        } else {
+            return getAQuiz(id);
+        }
     }
 
     @GetMapping(path = "api/quizzes")
