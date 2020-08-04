ea31536e7e74a578813346b3b0fa0db2794e3190
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-08-04 10:01:02 +0000

    get answer from the quiz object and compare with user answer

--- Web Quiz Engine/task/src/engine/SolveAQuizController.java
+++ Web Quiz Engine/task/src/engine/SolveAQuizController.java
@@ -1,19 +1,22 @@
 package engine;
 
-import org.springframework.http.HttpStatus;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.web.bind.annotation.*;
-
 @RestController
 public class SolveAQuizController {
     QuizResults quizResults = new QuizResults();
+    Quiz quiz = new Quiz();
+    public SolveAQuizController(){
+    }
 
     @PostMapping(path = "/api/quizzes/{id}/solve")
-    public QuizResults solveQuiz(@PathVariable int id, @RequestParam(value = "answer") int answer) throws QuizNotFoundException{
+    public QuizResults solveQuiz(@PathVariable int id, @RequestParam(value = "answer") int answer) throws QuizNotFoundException, JsonProcessingException {
         if (id < 0 || id > QuizDB.sizeOfQuizDB()) {
             throw new QuizNotFoundException();
         } else {
-            if (answer != 2) {
-                quizResults.setFeedback("Wrong answer! Please try again.");
+            //get the returned json from and retrieve the answer and do the comparism here
+            if (QuizDB.getAQuiz(id).getAnswer() != answer) {
+                quizResults.setFeedback(" Wrong answer! Please try again.");
                 quizResults.setSuccess(false);
             } else {
                 quizResults.setFeedback("Congratulations, you're right!");
