616a2f39b49be10531864a4292fa818353f0286e
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-08-07 10:02:35 +0000

    add annotation to the size of options to a min of 2

--- Web Quiz Engine/task/src/engine/Quiz.java
+++ Web Quiz Engine/task/src/engine/Quiz.java
@@ -3,14 +3,18 @@ package engine;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Component;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Component
 public class Quiz {
-
     private int id;
     private String title;
     private String text;
+    @Size(min = 2)
     private String[] options;
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private int[] answer;
